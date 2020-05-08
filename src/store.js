04ddd5e2033fe5fbfd0a475afb6dd08447a23ca3
bhar4t <seizedbharat@gmail.com>
2020-05-08 14:53:11 +0530

    working fine with react!

---
+++ src/store.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase";
+import "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/messaging";
+import "firebase/functions";
+
+let app = firebase.initializeApp({
+  apiKey: "AIzaSyC0s2xwTwLSqtCJYJc8Zub1Otiao-Y5NaI",
+  authDomain: "web-firebase-rtc.firebaseapp.com",
+  databaseURL: "https://web-firebase-rtc.firebaseio.com",
+  projectId: "web-firebase-rtc",
+  storageBucket: "web-firebase-rtc.appspot.com",
+  messagingSenderId: "694948618954",
+  appId: "1:694948618954:web:7a3a446a1624f839258a33",
+});
+
+const db = app.firestore();
+
+export default firebase;
+export { db };
