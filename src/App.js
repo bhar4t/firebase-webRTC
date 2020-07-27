0b192c0b70a57a9004a2e2baf4c3c3a42ab424f0
bhar4t <seizedbharat@gmail.com>
2020-07-27 22:33:55 +0530

    full height vid

--- src/App.js
+++ src/App.js
@@ -263,6 +263,51 @@ function App() {
     });
   }
 
+  React.useEffect(() => {
+    dragElement(document.getElementById("mydiv"));
+
+    function dragElement(elmnt) {
+      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+      if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+      } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+      }
+
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      }
+
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
+  }, [])
+
   return (
     <>
       <div id="buttons">
@@ -278,15 +323,27 @@ function App() {
         </button>
       </div>
       <div id="currentRoom"></div>
-      <div id="videos">
-        <video
-          ref={localVideo}
-          id="localVideo"
-          muted
-          autoPlay
-          playsInline
-        ></video>
-        <video ref={remoteVideo} id="remoteVideo" autoPlay playsInline></video>
+      <div style={{ height: window.innerHeight, width: window.innerWidth, backgroundColor: 'gray' }} >
+
+        <div id="mydiv">
+          <div id="mydivheader">Move</div>
+          <video
+            style={{ backgroundColor: 'black', height: 130, width: 'auto', objectFit: 'contain' }}
+            ref={localVideo}
+            id="localVideo"
+            muted
+            autoPlay
+            playsInline
+          ></video>
+        </div>
+
+        <video style={{
+          backgroundColor: 'black',
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          objectFit: 'contain'
+        }} ref={remoteVideo} id="remoteVideo" autoPlay playsInline></video>
       </div>
     </>
   );
