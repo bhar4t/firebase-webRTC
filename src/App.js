c61679d965cf3cf0dc44f29ca53c546510f956a9
bhar4t <seizedbharat@gmail.com>
2021-02-04 17:13:53 +0530

    footer strip more glassy

--- src/App.js
+++ src/App.js
@@ -356,12 +356,12 @@ function App() {
         alignItems: 'center',
         height: 100,
         width: window.innerWidth,
-        background: 'rgba( 255, 255, 255, 0.25 )',
-        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
-        backdropFilter: 'blur( 3.0px )',
-        webkitBackdropFilter: 'blur( 3.0px )',
+        "background": "rgba( 255, 255, 255, 0.05 )",
+        "boxShadow": "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+        "backdropFilter": "blur( 2.0px )",
+        "WebkitBackdropFilter": "blur( 2.0px )",
         borderRadius: '10px 10px 0px 0px',
-        border: '1px solid rgba( 255, 255, 255, 0.18 )',
+        "border": "1px solid rgba( 255, 255, 255, 0.18 )"
       }}>
         <button disabled id="createBtn">
           Create Meet
